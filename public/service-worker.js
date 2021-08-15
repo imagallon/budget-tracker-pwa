7cd8e18fa357e0cd81e668e0953e6a3ec93eeaa0
Izzy <browncans@gmail.com>
2021-08-14 17:30:06 -0700

    changes in service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,11 +13,6 @@ const FILES_TO_CACHE = [
 
 // install
 self.addEventListener("install", function (evt) {
-  // evt.waitUntil(
-  //   caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
-  // );
-
-  // pre cache all static assets
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
@@ -55,7 +50,6 @@ self.addEventListener("fetch", function (evt) {
         .then((cache) => {
           return fetch(evt.request)
             .then((response) => {
-              // If the response was good, clone it and store it in the cache.
               if (response.status === 200) {
                 cache.put(evt.request.url, response.clone());
               }
@@ -63,13 +57,12 @@ self.addEventListener("fetch", function (evt) {
               return response;
             })
             .catch((err) => {
-              // Network request failed, try to get it from the cache.
               return cache.match(evt.request);
             });
         })
         .catch((err) => console.log(err))
     );
-    return; // return so that no else block is needed
+    return;
   }
 
   evt.respondWith(
